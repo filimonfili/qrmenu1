6489ef16f5c97acf54a92ad437c3e7ca38cbd4da
filimonfili <filpac1995@gmail.com>
2024-06-26 22:00:34 +0300

    css updated

--- qrmenu1/settings.py
+++ qrmenu1/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT= os.path.join(BASE_DIR,'assets')
 MEDIA_URL='media/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
